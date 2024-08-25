1b07a8b88329a08a833f2d819f8c8c430d466fe7
Fahmudul Hassan <fahmudul234@gmail.com>
2024-08-25 11:04:28 +0600

    simple ui implemented in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import errorpage from "../../public/error.gif";
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="container mx-auto min-h-screen flex flex-col justify-center items-center w-full">
+      <Image alt="error" src={errorpage} className="w-1/3" />
+      <Link className="btn my-5" href="/">
+        Return Home
+      </Link>
     </div>
   );
 }
